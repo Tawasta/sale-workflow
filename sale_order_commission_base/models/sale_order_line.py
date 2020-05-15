26ce5d48c4c11e875e2f68da8dbe74e647fde8d1
difbit <timo.kekalainen@hotmail.com>
2020-05-15 13:49:57 +0300

    [ADD] Adds field commission

---
+++ sale_order_commission_base/models/sale_order_line.py
@@ -0,0 +1,24 @@
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    commission = fields.Float(
+        string="Commission",
+        compute="_compute_commission",
+        search="_search_commission",
+    )
+
+    @api.multi
+    def _search_commission(self, operator, value):
+        recs = self.search([]).filtered(
+            lambda x: x.commission)
+        if recs:
+            return [('id', 'in', [x.id for x in recs])]
+
+    def _compute_commission(self):
+        for line in self:
+            line.commission = line.price_unit * line.product_uom_qty - line.discount
