e49329e13ddb108f123bac6aac2087bba16040a6
difbit <timo.kekalainen@hotmail.com>
2020-06-18 16:06:44 +0300

    [IMP] Helpful comments added

--- sale_order_pricelist_price_by_categ_qty/models/sale_order.py
+++ sale_order_pricelist_price_by_categ_qty/models/sale_order.py
@@ -7,29 +7,41 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def compute_global_discount(self):
+        """Button for calculating unit prices based on pricelist product quantities"""
         pricelist = self.pricelist_id
+        # Create empty recordset for sale order line, this is used in dictionary
         sale_line_model = self.env['sale.order.line']
         categ_products = {}
 
+        # First loop through sale order lines and fetch product categories and
+        # sale order line records
         for line in self.order_line:
             product_category = line.product_id.categ_id
             if product_category:
                 qty = categ_products.get(
                     product_category, [0, list(sale_line_model)]
                 )
+                # Result is a Python dictionary where keys are product categories
+                # and values are sale order line records
                 categ_products[product_category] = [
                     qty[0] + line.product_uom_qty, qty[1] + [line]
                 ]
 
+        # items are records within pricelist
         items = pricelist.mapped('item_ids')
 
+        # Loop through product categories
         for categ in categ_products:
+            # Loop through sale order line records
             for line in categ_products.get(categ)[1]:
                 biggest_qty = max(items.mapped('min_quantity'))
                 smallest_qty = 0
                 index = 0
                 item_price = 0
+                # Keep looping until pricelist item with largest appropriate quantity
+                # is fetched
                 while smallest_qty < biggest_qty:
+                    # Break the loop when all items have been checked
                     if index == len(items):
                         break
                     item = items[index]
@@ -40,6 +52,8 @@ class SaleOrder(models.Model):
                             item.product_tmpl_id ==
                             line.product_id.product_tmpl_id):
                         smallest_qty = item.min_quantity
+                        # price_surcharge-field is item's added price and this is
+                        # used to substitute sale order line's unit price
                         item_price = item.price_surcharge
                     index += 1
                 if item_price:
