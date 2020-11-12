b6f0f077d6a35efdfb44f2cdb583a0cbaf0a1253
difbit <timo.kekalainen@hotmail.com>
2020-11-12 10:20:57 +0200

    [IMP] Comment added

--- sale_order_line_view_analytic_tags/models/sale_order_line.py
+++ sale_order_line_view_analytic_tags/models/sale_order_line.py
@@ -6,4 +6,5 @@ class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
 
+    # Set store to True so that the grouping works. Else Odoo gives an error
     analytic_tag_name = fields.Char(related='analytic_tag_ids.name', store=True)
